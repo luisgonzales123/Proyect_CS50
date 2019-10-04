1c592f07fd8325e3e69677cff9f700e6cd5a3fc6
Gabx01 <lgabrielgonzaleslopez.lggl@gmail.com>
2019-10-04 13:43:37 -0600

    mejorando

--- js/conexion.js
+++ js/conexion.js
@@ -1,13 +1,18 @@
 var serialPort = require('serialport');
 
-var serial = new serialPort('COM11',function(err){
-    console.log("Conexion lista");
-},{
-    parser:serialPort.
+var serial = new serialPort('COM11',{parser: serialPort.parsers.readline('\n')});
+
+serial.on('open',function(err){
+    if(err != null){
+        console.log(err);
+    }else{
+        console.log("Conexion exitosa");
+    }
 });
 
-serial.on('data',function(){
+serial.on('data',function(data){
     console.log(data.toString());
+   // $('#text-presion').text(data.toString());
 });
 
 
